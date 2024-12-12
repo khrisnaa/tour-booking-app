94aaba18aa6c8d5dfb72b56c87b59fa3b8e88839
khrisnaa <109814050+khrisnaa@users.noreply.github.com>
2024-12-12 17:23:50 +0800

    feat: create initial route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -12,4 +13,20 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+
+    Route::resource('categories', Controller::class);
+
+    Route::resource('tour-packages', Controller::class);
+
+    Route::resource('tour-photos', Controller::class);
+
+    Route::resource('reviews', Controller::class);
+
+    Route::resource('banks', Controller::class);
+
+    Route::resource('bookings', Controller::class);
+
+});
+
+require __DIR__ . '/auth.php';
