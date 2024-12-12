eb9c9d7cd0c94adaff5343960a676813cf3cb25c
khrisnaa <109814050+khrisnaa@users.noreply.github.com>
2024-12-12 17:39:34 +0800

    feat: create tour package model and migration

---
+++ database/migrations/2024_12_12_093208_create_tour_packages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tour_packages', function (Blueprint $table) {
+            $table->uuid('id')->primary()->unique();
+            $table->foreignUuid('category_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price');
+            $table->date('duration_days');
+            $table->date('duration_hours');
+            $table->string('location');
+            $table->string('thumbnail');
+            $table->string('slug');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tour_packages');
+    }
+};
