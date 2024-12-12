591f68090f122da2978ef72b9c7729038bd6e67b
khrisnaa <109814050+khrisnaa@users.noreply.github.com>
2024-12-12 19:36:39 +0800

    feat: create fillable for booking

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -11,6 +11,24 @@ class Booking extends Model
 {
     use HasUuids, SoftDeletes;
 
+    protected $fillable = [
+        'id',
+        'tour_package_id',
+        'bank_id',
+        'start_date',
+        'end_date',
+        'quantity',
+        'subtotal',
+        'insurance',
+        'tax',
+        'total',
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'status',
+        'proof',
+    ];
     public function tourPackage(): BelongsTo
     {
         return $this->belongsTo(TourPackage::class);
