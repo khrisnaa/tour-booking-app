23a00fc391e1e440e569829c806dbf77b255b073
khrisnaa <igdkhrisnaaa@gmail.com>
2025-01-10 18:52:48 +0800

    fix: mobile navigation button

--- resources/views/components/public/bottom-navigation.blade.php
+++ resources/views/components/public/bottom-navigation.blade.php
@@ -1,6 +1,6 @@
 <div class="fixed bottom-0 left-0 w-full px-6 py-8">
     <div class="flex h-16 w-full items-center justify-between rounded-full bg-black p-2 px-4">
-        <x-public.partials.navigation-button :active="request()->routeIs('home')">
+        <x-public.navigation-button :active="request()->routeIs('home')">
             <svg class="lucide lucide-house"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -15,9 +15,9 @@
                 <path
                     d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
             </svg>
-        </x-public.partials.navigation-button>
+        </x-public.navigation-button>
 
-        <x-public.partials.navigation-button :active="request()->routeIs('admin.tour-packages*')">
+        <x-public.navigation-button :active="request()->routeIs('admin.tour-packages*')">
             <svg class="lucide lucide-map"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -33,9 +33,9 @@
                 <path d="M15 5.764v15" />
                 <path d="M9 3.236v15" />
             </svg>
-        </x-public.partials.navigation-button>
+        </x-public.navigation-button>
 
-        <x-public.partials.navigation-button :active="request()->routeIs('admin.tour-packages*')">
+        <x-public.navigation-button :active="request()->routeIs('admin.tour-packages*')">
             <svg class="lucide lucide-heart"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -49,9 +49,9 @@
                 <path
                     d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
             </svg>
-        </x-public.partials.navigation-button>
+        </x-public.navigation-button>
 
-        <x-public.partials.navigation-button :active="request()->routeIs('admin.tour-packages*')">
+        <x-public.navigation-button :active="request()->routeIs('admin.tour-packages*')">
             <svg class="lucide lucide-align-justify"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -66,6 +66,6 @@
                 <path d="M3 18h18" />
                 <path d="M3 6h18" />
             </svg>
-        </x-public.partials.navigation-button>
+        </x-public.navigation-button>
     </div>
 </div>
