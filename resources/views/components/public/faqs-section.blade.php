fa9d1f2692ca8069266918b04855087cb85aab61
khrisnaa <igdkhrisnaaa@gmail.com>
2025-01-13 15:53:09 +0800

    feat: update navigation padding

--- resources/views/components/public/faqs-section.blade.php
+++ resources/views/components/public/faqs-section.blade.php
@@ -9,58 +9,16 @@
 
     <div class="mx-auto max-w-2xl">
         <!-- Accordion -->
-        <div class="hs-accordion-group divide-y divide-gray-200">
-            <div class="hs-accordion active pb-3"
-                id="hs-basic-with-title-and-arrow-stretched-heading-one">
-                <button
-                    class="hs-accordion-toggle group inline-flex w-full items-center justify-between gap-x-3 rounded-lg pb-3 text-start font-semibold text-gray-800 transition hover:text-gray-500 focus:text-gray-500 focus:outline-none md:text-lg"
-                    aria-expanded="true"
-                    aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-one">
-                    Can I cancel at anytime?
-                    <svg class="block size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:hidden"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="m6 9 6 6 6-6" />
-                    </svg>
-                    <svg class="hidden size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:block"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="m18 15-6-6-6 6" />
-                    </svg>
-                </button>
-                <div class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
-                    id="hs-basic-with-title-and-arrow-stretched-collapse-one"
-                    role="region"
-                    aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-one">
-                    <p class="text-gray-600">
-                        Yes, you can cancel anytime no questions are asked while you cancel but we would highly
-                        appreciate if you will give us some feedback.
-                    </p>
-                </div>
-            </div>
-
+        <div class="hs-accordion-group divide-y divide-gray-800 border-b border-b-gray-800">
             <div class="hs-accordion pb-3 pt-6"
                 id="hs-basic-with-title-and-arrow-stretched-heading-two">
                 <button
-                    class="hs-accordion-toggle group inline-flex w-full items-center justify-between gap-x-3 rounded-lg pb-3 text-start font-semibold text-gray-800 transition hover:text-gray-500 focus:text-gray-500 focus:outline-none md:text-lg"
+                    class="hs-accordion-toggle group inline-flex w-full items-start justify-between gap-x-3 rounded-lg pb-3 text-start font-mont font-semibold uppercase text-gray-800 transition hover:text-gray-500 focus:text-gray-500 focus:outline-none sm:items-center md:text-lg"
                     aria-expanded="false"
                     aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-two">
                     My team has credits. How do we use them?
                     <svg class="block size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:hidden"
+                        class="lucide lucide-plus"
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
                         height="24"
@@ -70,9 +28,11 @@
                         stroke-width="2"
                         stroke-linecap="round"
                         stroke-linejoin="round">
-                        <path d="m6 9 6 6 6-6" />
+                        <path d="M5 12h14" />
+                        <path d="M12 5v14" />
                     </svg>
                     <svg class="hidden size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:block"
+                        class="lucide lucide-minus"
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
                         height="24"
@@ -82,7 +42,7 @@
                         stroke-width="2"
                         stroke-linecap="round"
                         stroke-linejoin="round">
-                        <path d="m18 15-6-6-6 6" />
+                        <path d="M5 12h14" />
                     </svg>
                 </button>
                 <div class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
@@ -92,181 +52,6 @@
                     <p class="text-gray-600">
                         Once your team signs up for a subscription plan. This is where we sit down, grab a cup of coffee
                         and dial in the details.
-                    </p>
-                </div>
-            </div>
-
-            <div class="hs-accordion pb-3 pt-6"
-                id="hs-basic-with-title-and-arrow-stretched-heading-three">
-                <button
-                    class="hs-accordion-toggle group inline-flex w-full items-center justify-between gap-x-3 rounded-lg pb-3 text-start font-semibold text-gray-800 transition hover:text-gray-500 focus:text-gray-500 focus:outline-none md:text-lg"
-                    aria-expanded="false"
-                    aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-three">
-                    How does Preline's pricing work?
-                    <svg class="block size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:hidden"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="m6 9 6 6 6-6" />
-                    </svg>
-                    <svg class="hidden size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:block"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="m18 15-6-6-6 6" />
-                    </svg>
-                </button>
-                <div class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
-                    id="hs-basic-with-title-and-arrow-stretched-collapse-three"
-                    role="region"
-                    aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-three">
-                    <p class="text-gray-600">
-                        Our subscriptions are tiered. Understanding the task at hand and ironing out the wrinkles is
-                        key.
-                    </p>
-                </div>
-            </div>
-
-            <div class="hs-accordion pb-3 pt-6"
-                id="hs-basic-with-title-and-arrow-stretched-heading-four">
-                <button
-                    class="hs-accordion-toggle group inline-flex w-full items-center justify-between gap-x-3 rounded-lg pb-3 text-start font-semibold text-gray-800 transition hover:text-gray-500 focus:text-gray-500 focus:outline-none md:text-lg"
-                    aria-expanded="false"
-                    aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-four">
-                    How secure is Preline?
-                    <svg class="block size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:hidden"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="m6 9 6 6 6-6" />
-                    </svg>
-                    <svg class="hidden size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:block"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="m18 15-6-6-6 6" />
-                    </svg>
-                </button>
-                <div class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
-                    id="hs-basic-with-title-and-arrow-stretched-collapse-four"
-                    role="region"
-                    aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-four">
-                    <p class="text-gray-600">
-                        Protecting the data you trust to Preline is our first priority. This part is really crucial in
-                        keeping the project in line to completion.
-                    </p>
-                </div>
-            </div>
-
-            <div class="hs-accordion pb-3 pt-6"
-                id="hs-basic-with-title-and-arrow-stretched-heading-five">
-                <button
-                    class="hs-accordion-toggle group inline-flex w-full items-center justify-between gap-x-3 rounded-lg pb-3 text-start font-semibold text-gray-800 transition hover:text-gray-500 focus:text-gray-500 focus:outline-none md:text-lg"
-                    aria-expanded="false"
-                    aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-five">
-                    How do I get access to a theme I purchased?
-                    <svg class="block size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:hidden"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="m6 9 6 6 6-6" />
-                    </svg>
-                    <svg class="hidden size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:block"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="m18 15-6-6-6 6" />
-                    </svg>
-                </button>
-                <div class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
-                    id="hs-basic-with-title-and-arrow-stretched-collapse-five"
-                    role="region"
-                    aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-five">
-                    <p class="text-gray-600">
-                        If you lose the link for a theme you purchased, don't panic! We've got you covered. You can
-                        login to your account, tap your avatar in the upper right corner, and tap Purchases. If you
-                        didn't create a login or can't remember the information, you can use our handy Redownload page,
-                        just remember to use the same email you originally made your purchases with.
-                    </p>
-                </div>
-            </div>
-
-            <div class="hs-accordion pb-3 pt-6"
-                id="hs-basic-with-title-and-arrow-stretched-heading-six">
-                <button
-                    class="hs-accordion-toggle group inline-flex w-full items-center justify-between gap-x-3 rounded-lg pb-3 text-start font-semibold text-gray-800 transition hover:text-gray-500 focus:text-gray-500 focus:outline-none md:text-lg"
-                    aria-expanded="false"
-                    aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-six">
-                    Upgrade License Type
-                    <svg class="block size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:hidden"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="m6 9 6 6 6-6" />
-                    </svg>
-                    <svg class="hidden size-5 shrink-0 text-gray-600 group-hover:text-gray-500 hs-accordion-active:block"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="m18 15-6-6-6 6" />
-                    </svg>
-                </button>
-                <div class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
-                    id="hs-basic-with-title-and-arrow-stretched-collapse-six"
-                    role="region"
-                    aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-six">
-                    <p class="text-gray-600">
-                        There may be times when you need to upgrade your license from the original type you purchased
-                        and we have a solution that ensures you can apply your original purchase cost to the new license
-                        purchase.
                     </p>
                 </div>
             </div>
--- resources/views/components/public/footer.blade.php
+++ resources/views/components/public/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="flex flex-wrap items-center justify-between gap-2">
             <div>
                 <p class="text-xs text-gray-600">
-                    © 2025 Preline Labs.
+                    © 2025 WeBali Tour.
                 </p>
             </div>
             <!-- End Col -->
@@ -31,56 +31,7 @@
                         Github
                     </a>
                 </li>
-                <li class="inline-block">
-                    <!-- Dark Mode -->
-                    <button
-                        class="hs-dark-mode relative flex size-7 items-center justify-center rounded-full border border-gray-200 text-gray-500 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none hs-dark-mode-active:hidden"
-                        data-hs-theme-click-value="dark"
-                        type="button">
-                        <span class="sr-only">Dark</span>
-                        <svg class="size-3.5 shrink-0"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round">
-                            <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
-                        </svg>
-                    </button>
-                    <button
-                        class="hs-dark-mode relative flex hidden size-7 items-center justify-center rounded-full border border-gray-200 text-gray-500 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none hs-dark-mode-active:flex"
-                        data-hs-theme-click-value="light"
-                        type="button">
-                        <span class="sr-only">Light</span>
-                        <svg class="size-3.5 shrink-0"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round">
-                            <circle cx="12"
-                                cy="12"
-                                r="4"></circle>
-                            <path d="M12 2v2"></path>
-                            <path d="M12 20v2"></path>
-                            <path d="m4.93 4.93 1.41 1.41"></path>
-                            <path d="m17.66 17.66 1.41 1.41"></path>
-                            <path d="M2 12h2"></path>
-                            <path d="M20 12h2"></path>
-                            <path d="m6.34 17.66-1.41 1.41"></path>
-                            <path d="m19.07 4.93-1.41 1.41"></path>
-                        </svg>
-                    </button>
-                    <!-- End Dark Mode -->
-                </li>
+
             </ul>
             <!-- End List -->
         </div>
